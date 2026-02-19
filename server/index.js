4eb80876df6982ffe4c6a4da25b398578a4846e0
yash-mandali <yashmandali211@gmail.com>
2026-02-19 12:57:48 +0530

    ...

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
-const CORS_ORIGIN = 'https://editor-portfolio-back.vercel.app';
+// const CORS_ORIGIN = 'https://editor-portfolio-back.vercel.app';
 
 // Middleware
 // app.use(cors({
