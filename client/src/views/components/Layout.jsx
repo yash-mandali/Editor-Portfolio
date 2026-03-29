ef7eccae6fd208f95a0560e76b08a7c863ebd49b
yash-mandali <yashmandali211@gmail.com>
2026-03-29 17:01:37 +0530

    removed

--- client/src/views/components/Layout.jsx
+++ client/src/views/components/Layout.jsx
@@ -223,6 +223,7 @@ export const Layout = ({ children }) => {
 
   return (
     <div className="min-h-screen bg-neutral-50 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100 font-sans selection:bg-amber-500/30 transition-colors duration-300">
+      {console.log('Layout is rendering AnimatedBackground')}
       <AnimatedBackground />
       <Navbar />
       {showLoader && <Loader />}
