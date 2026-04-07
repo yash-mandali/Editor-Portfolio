141a33af590efa9c28961f68ea3792516df8873d
yash-mandali <yashmandali211@gmail.com>
2026-04-07 18:53:48 +0530

    videoadededeedd

--- client/src/views/pages/Home.jsx
+++ client/src/views/pages/Home.jsx
@@ -195,7 +195,7 @@ const Home = () => {
               </MagneticBtn>
 
               <MagneticBtn
-                onClick={() => setSelectedVideo('https://www.youtube.com/watch?v=zF9m02WllZc')}
+                onClick={() => setSelectedVideo('https://drive.google.com/file/d/1RwAEv9k__DwPH5tucPrCbJ_VwJrF9Wlt/preview')}
                 className="group flex items-center gap-4 text-white/70 font-black tracking-[0.3em] uppercase text-[10px] hover:text-[#00d4ff] transition-colors font-cinzel"
               >
                 <div className="w-14 h-14 rounded-full border border-white/10 flex items-center justify-center group-hover:border-[#00d4ff]/40 group-hover:bg-[#00d4ff]/5 transition-all">
